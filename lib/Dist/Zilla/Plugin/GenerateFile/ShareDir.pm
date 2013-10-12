e7e183fc6f236a6461a80680d5482108833bc667
Karen Etheridge <ether@cpan.org>
2013-10-12 01:29:11 -0700

    initial code, docs, tests

--- lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
+++ lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
@@ -1,33 +1,170 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::GenerateFile::ShareDir;
-# ABSTRACT: ...
+# ABSTRACT: Create files in the build, based on a template located in a dist sharedir
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with (
+    'Dist::Zilla::Role::FileGatherer',
+    'Dist::Zilla::Role::FileMunger',
+    'Dist::Zilla::Role::TextTemplate',
+);
 
+use MooseX::SlurpyConstructor;
+use Scalar::Util 'blessed';
+use File::ShareDir 'dist_file';
+use Path::Tiny;
+use namespace::autoclean;
 
-1;
+has dist => (
+    is => 'ro', isa => 'Str',
+    init_arg => '-dist',
+    lazy => 1,
+    default => sub { (my $dist = blessed(shift)) =~ s/::/-/g; $dist },
+);
+
+has filename => (
+    init_arg => '-destination_filename',
+    is => 'ro', isa => 'Str',
+    required => 1,
+);
+
+has source_filename => (
+    init_arg => '-source_filename',
+    is => 'ro', isa => 'Str',
+    lazy => 1,
+    default => sub { shift->filename },
+);
+
+has _extra_args => (
+    isa => 'HashRef[Str]',
+    init_arg => undef,
+    lazy => 1,
+    default => sub { {} },
+    traits => ['Hash'],
+    handles => { _extra_args => 'elements' },
+    slurpy => 1,
+);
+
+around BUILDARGS => sub
+{
+    my $orig = shift;
+    my $class = shift;
+
+    my $args = $class->$orig(@_);
+    $args->{'-destination_filename'} = delete $args->{'-filename'} if exists $args->{'-filename'};
+
+    return $args;
+};
+
+around dump_config => sub
+{
+    my ($orig, $self) = @_;
+    my $config = $self->$orig;
+
+    $config->{'' . __PACKAGE__} = {
+        '-dist' => $self->dist,
+        '-source_filename' => $self->source_filename,
+        '-destination_filename' => $self->filename,
+        $self->_extra_args,
+    };
+    return $config;
+};
+
+sub gather_files
+{
+    my $self = shift;
+
+    require Dist::Zilla::File::InMemory;
+
+    # this should die if the file does not exist
+    my $file = dist_file($self->dist, $self->source_filename);
+
+    $self->add_file(Dist::Zilla::File::InMemory->new(
+        name => $self->filename,
+        # TODO: slurp_utf8 and set encoding=utf8
+        content => path($file)->slurp_raw,
+    ));
+}
+
+sub munge_file
+{
+    my ($self, $file) = @_;
+
+    return unless $file->name eq $self->filename;
+    $self->log_debug([ 'updating contents of %s in memory', $file->name ]);
+
+    my $content = $file->content;
+    $file->content(
+        $self->fill_in_string(
+            $content,
+            {
+                $self->_extra_args,     # must be first
+                dist => \($self->zilla),
+                plugin => \$self,
+            },
+        )
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =pod
 
+=for Pod::Coverage::TrustPod
+    gather_files
+    munge_file
+
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::GenerateFile::ShareDir;
+In your F<dist.ini>:
 
-    ...
+    [GenerateFile::ShareDir]
+    -dist = Dist::Zilla::PluginBundle::Author::ME
+    -source_filename = my_data_template.txt
+    -destination_filename = examples/my_data.txt
+    key1 = value to pass to template
+    key2 = another value to pass to template
 
 =head1 DESCRIPTION
 
-...
+Generates a file in your dist, indicated by C<-destination_file>, based on the
+L<Text::Template> located in the C<-source_file> of C<-dist>'s
+L<distribution sharedir|File::ShareDir>. Any extra config values are passed
+along to the template, in addition to C<$zilla> and C<$plugin> objects.
 
-=head1 FUNCTIONS/METHODS
+I expect that usually the C<-dist> that contains the template will be either a
+plugin bundle, so you can generate a custom-tailored file in your dist, or a
+plugin that subclasses this one.
+
+=head1 OPTIONS
+
+This plugin accepts the following options:
 
 =over 4
 
-=item * C<foo>
+=item * C<-dist>
 
-...
+=for stopwords sharedir
+
+The distribution name to use when finding the sharedir (see L<File::ShareDir>
+and L<Dist::Zilla::Plugin::ShareDir>). Defaults to the dist corresponding to
+the running plugin.
+
+=item * C<-destination_filename> or C<-filename>
+
+The filename to generate in the distribution being built. Required.
+
+=item * C<-source_filename>
+
+The filename in the sharedir to use to generate the new file. Defaults to the
+same filename and path as C<-destination_file>.
+
+=item * C<arbitrary option>
+
+All other keys/values provided will be passed to the template as is.
 
 =back
 
@@ -39,15 +176,13 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Plugin-GenerateFile-ShareDir@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-GenerateFile-ShareDir@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<File::ShareDir>
+* L<Dist::Zilla::Plugin::ShareDir>
+* L<Text::Template>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,44 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::GenerateFile::ShareDir;
+use Test::DZil;
+use Path::Tiny;
+use utf8;
+binmode $_, ':utf8' foreach map { Test::Builder->new->$_ } qw(output failure_output);
 
-fail('this test is TODO!');
+use Test::File::ShareDir
+    -share => {
+        -dist => { 'Some-Other-Dist' => 't/corpus' },
+    };
+
+my $tzil = Builder->from_config(
+    { dist_root => 't/corpus/basic' },
+    {
+        add_files => {
+            'source/dist.ini' => simple_ini(
+                [ 'GenerateFile::ShareDir' => {
+                    '-dist' => 'Some-Other-Dist',
+                    '-source_filename' => 'template.txt',
+                    '-destination_filename' => 'data/useless_file.txt',
+                    numero => 'neuf',
+                } ],
+            ),
+        },
+    },
+);
+
+$tzil->build;
+
+my $build_dir = $tzil->tempdir->subdir('build');
+my $file = path($build_dir, 'data', 'useless_file.txt');
+ok(-e $file, 'file created');
+
+my $content = $file->slurp_utf8;
+
+my $zilla_version = Dist::Zilla->VERSION;
+
+like($content, qr/^This file was generated with Dist::Zilla::Plugin::GenerateFile::ShareDir /, '$plugin is passed to the template');
+like($content, qr/Dist::Zilla $zilla_version/, '$zilla is passed to the template');
+like($content, qr/Le numéro de Maurice Richard est neuf./, 'arbitrary args are passed to the template');
 
 done_testing;
