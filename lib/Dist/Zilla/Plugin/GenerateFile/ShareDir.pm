4454d2732438033cba7108492524e86ab446f4a1
Karen Etheridge <ether@cpan.org>
2015-07-04 22:27:41 -0700

    increment $VERSION after 0.006 release

--- lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
+++ lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::GenerateFile::ShareDir;
 # KEYWORDS: plugin distribution generate create file sharedir template
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 
 use Moose;
 with (
