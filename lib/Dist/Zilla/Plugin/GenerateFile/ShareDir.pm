27930a08af9b8acc89e2c80bf41e86d3aeab3e34
Karen Etheridge <ether@cpan.org>
2015-11-13 18:24:09 -0800

    increment $VERSION after 0.009 release

--- lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
+++ lib/Dist/Zilla/Plugin/GenerateFile/ShareDir.pm
@@ -4,7 +4,7 @@ package Dist::Zilla::Plugin::GenerateFile::ShareDir;
 # ABSTRACT: (DEPRECATED) Create files in the repository or in the build, based on a template located in a dist sharedir
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 use Moose;
 extends 'Dist::Zilla::Plugin::GenerateFile::FromShareDir';
