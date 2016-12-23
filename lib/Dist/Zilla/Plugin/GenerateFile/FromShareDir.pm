6cd62be45687ef653f95ca3f5a97d8c547d80c6f
Karen Etheridge <ether@cpan.org>
2016-12-22 17:11:00 -0800

    increment $VERSION after 0.013 release

--- lib/Dist/Zilla/Plugin/GenerateFile/FromShareDir.pm
+++ lib/Dist/Zilla/Plugin/GenerateFile/FromShareDir.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::GenerateFile::FromShareDir;
 # ABSTRACT: Create files in the repository or in the build, based on a template located in a dist sharedir
 # KEYWORDS: plugin distribution generate create file sharedir template
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 use Moose;
 with (
